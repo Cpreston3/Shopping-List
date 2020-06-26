e71aa79f1a70ed62d33133b9aeeee57ea6339b21
Casey Preston <caseypreston4@gmail.com>
2020-06-25 21:25:43 -0600

    draft 1

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onLoad = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+    console.log(form);}
+
+/* form.addEventListener('submit',(event) => {
+     handleItemForm(event);
+ });
+
+}
+function handleItemForm(event, formRef ) {
+ if(e.preventDefault()){
+     e.preventDefault();
+ }
+ console.log("event:", event);
+ return false;
+
+}*/
